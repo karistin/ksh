abdb1b400693d0dc310f0a408d6cf084174791a0
karistin <sungjun1109@gmail.com>
2022-02-18 02:26:07 +0900

    arrow-key-function-re

--- bash.py
+++ bash.py
@@ -2,18 +2,20 @@
 import os
 import signal
 import sys
-import curses
+
 
 """
-1. local var 
+1. local var
 2. export err (re)
-err case 
+err case
     echo $A
 3. local var tests
-4. stream : 시간에 흐름에 따라 이용가능한 데이터 원소의 수열 
+4. stream : 시간에 흐름에 따라 이용가능한 데이터 원소의 수열
 (과거나 마래의 데이터를 읽지 못한다.)
 """
-local_dict={}
+
+local_dict = {}
+
 
 def get_env(cmd):
     env_paths = os.getenv('PATH').split(":")
@@ -21,6 +23,7 @@ def get_env(cmd):
         if os.path.isfile(f'{env_path}/{cmd}'):
             return f'{env_path}/{cmd}'
     return 0
+
 
 def changedir(args):
     if len(args) == 1:
@@ -34,28 +37,30 @@ def changedir(args):
             print(f'cd: no such file or directory: {args[1]}')
             return
 
+
 def exefile(args):
     filename = args[0][2:]
     if os.path.isfile(filename):
         pid = os.fork()
         if pid == 0:
-            os.execvp(args[0],[" "])
+            os.execvp(args[0], [" "])
         else:
-            os.waitpid(pid,0)
+            os.waitpid(pid, 0)
     else:
         print(f'ksh: no such file of directory : {args[0]}')
         return
 
+
 def export_var(cmds, args):
-    # print(cmds)
     if len(cmds) < 2:
         env = os.environ
         for key in env.keys():
             print(f'{key}={env[key]}')
     elif cmds[1].find("=") > 0:
-        words=args[1].split("=")
-        if len(words)==2:
+        words = args[1].split("=")
+        if len(words) == 2:
             os.environ[words[0]] = words[1]
+
 
 def unset(key):
     if os.getenv(key[1]):
@@ -66,10 +71,12 @@ def unset(key):
     else:
         print("ksh : no unset environ")
 
+
 def local_var(cmds):
-    words=cmds[1].split("=")
-    if len(words)==2:
+    words = cmds[1].split("=")
+    if len(words) == 2:
         local_dict[words[0]] = words[1]
+
 
 def print_set():
     env = os.environ
@@ -77,41 +84,39 @@ def print_set():
         print(f'{key}={env[key]}')
     for dict in local_dict.keys():
         print(f'{dict}={local_dict[dict]}')
-    #좀 다른듯?
+
 
 def main():
-    print(f'{os.getcwd()}') #pwd
+    print(f'{os.getcwd()}')  # pwd
     t = input("#> ")
-    if t == "exit": #종료
+    if t == "exit":  # 종료
         exit(0)
     if t == "":
         return 0
-    cmds = t.split(' ')  
+    cmds = t.split(' ')
     cmd = cmds[0]
     words = cmd.split("=")
     args = cmds[0:]
 
     if not args:
         args = [" "]
-    
     # FIXME: errors...
     for arg in args:
         if arg[0] == "$":
             # print(arg)
             if os.getenv(arg[1:]):
-                args[args.index(arg)] = os.environ[arg[1:]] # 환경 변수에서 찾기
+                args[args.index(arg)] = os.environ[arg[1:]]  # 환경 변수에서 찾기
             elif local_dict.get(arg[1:]):
                 args[args.index(arg)] = local_dict[arg[1:]]
-    #a=3
-    if len(words)==2:
+    if len(words) == 2:
         local_dict[words[0]] = words[1]
-    elif cmd == "cd": # cd
+    elif cmd == "cd":  # cd
         changedir(args)
-    elif cmd[:2] == "./": #shebang
+    elif cmd[:2] == "./":  # shebang
         exefile(args)
-    elif cmd =="export":
-        export_var(cmds , args) #export
-    elif cmd =="unset":
+    elif cmd == "export":
+        export_var(cmds, args)  # export
+    elif cmd == "unset":
         unset(args)
     elif cmd == "local":
         local_var(cmds)
@@ -125,10 +130,11 @@ def main():
             pid = os.fork()
 
             if pid == 0:
-                os.execvp(f'{path}', args )
+                os.execvp(f'{path}', args)
             else:
-                os.waitpid(pid,0)
-    print()#줄 한칸 뛰기
+                os.waitpid(pid, 0)
+    print()  # 줄 한칸 뛰기
+
 
 def Exit_gracefully(signal, frame):
     print("program shutdown")
--- term.py
+++ term.py
@@ -1,9 +1,28 @@
 #!/usr/bin/env python
 import termios
 import sys
-import tty
 from copy import deepcopy
 
+
+def timeout_mode(new, fd):
+    new[6][termios.VMIN] = 0
+    new[6][termios.VTIME] = 1
+    # read with timeout
+    # why => time out 시 또는 1byte 입력시
+    termios.tcsetattr(fd, termios.TCSADRAIN, new)
+    ch = sys.stdin.read(1)
+
+    if ch == "[":
+        ch = sys.stdin.read(1)
+        if ch == "B":    # down
+            ch = ""
+        elif ch == "C":  # right
+            ch = u"\u001b[1C"
+        elif ch == "A":  # up
+            ch = ""
+        elif ch == "D":  # left
+            ch = u"\u001b[1D"
+    return ch
 
 
 def set_mode():
@@ -20,31 +39,26 @@ def set_mode():
     new[6][termios.VMIN] = 1
     new[6][termios.VTIME] = 0
     # blocking read
+    # Minbyte 가 입력될때 까지 계속 진행
     try:
         # tty.setraw(fd)
         termios.tcsetattr(fd, termios.TCSADRAIN, new)
         ch = sys.stdin.read(1)
+        if ch == '\x1b':
+            ch = timeout_mode(new, fd)
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old)
-    return ch 
+    return ch
 
 
 def get_ch():
     ch = set_mode()
-    # if ch == '\x1b[D':
-    #     print("left")
-    # elif ch =='\x1b[C':
-    #     print ("right")
-    # elif ch=='\x1b[A':
-    #     print ("UP")
-    # elif ch=='\x1b[B':
-    #     print ("down")
-    # else:
     sys.stdout.write(ch)
     sys.stdout.flush()
-        # it will write everything in the buffer to the terminal,
+    #  it will write everything in the buffer to the terminal,
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     # for i in range(20):
     while(True):
         get_ch()
